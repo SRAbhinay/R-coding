d9b98924cc432a50fa1a7fca9a1b609da3d452e4
S Raj Abhinay <124809897+SRAbhinay@users.noreply.github.com>
2023-10-15 11:08:31 +0530

    Update 1.r

--- 1.r
+++ 1.r
@@ -31,7 +31,7 @@ median
 #Mode:
 m=which(frequency==max(frequency))
 m 
-fm=frequency[m]     # frequency of the modal class
+fm=frequency[m]     
 fm
 f1=frequency[m-1]  # frequency of the pre modal class
 f2=frequency[m+1] # frequency of the post modal class 
